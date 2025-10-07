0efe1f37f361838355e99a192b696482de5c177a
sneu14 <62690888+sneu14@users.noreply.github.com>
2025-10-07 09:18:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/bash
 
+if [ ! -d .venv ]; then
+        python3 -m venv .venv
+fi
+if [ -f .venv/bin/activate ]; then
+        source .venv/bin/activate
+else
+        echo "No valid venv-Enivironment available"
+        exit 1
+fi
+pip install -r requirements.txt
+
 export DISPLAY=:0.0
 source .venv/bin/activate
 python3 pymedia.py $1
